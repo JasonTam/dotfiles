bef70483d2b62af12ae9a439ac9cc275c6c955aa
Jason Tam <JasonTam22@gmail.com>
2015-03-26 19:19:21 -0400

    some mostly vanilla configs to start

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-2 --mode 1680x1050 --pos 0x0 --rotate left --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1680x1050 --pos 1050x328 --rotate normal --output TV-0 --off
