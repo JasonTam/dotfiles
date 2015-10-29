6c747c88f298e9b5498218fe2da9f7bf02d5a9f7
Jason Tam <JasonTam22@gmail.com>
2015-10-29 19:35:39 -0400

    Update setup.sh
    lol how do i bash

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sv DIR/.bash_aliases ~/.bash_aliases
-ln -sv DIR/.i3 ~/.i3
-ln -sv DIR/.screenlayout ~/.screenlayout
-ln -sv DIR/.vim ~/.vim
-ln -sv DIR/.vimrc ~/.vimrc
+ln -sv $DIR/.bash_aliases ~/.bash_aliases
+ln -sv $DIR/.i3 ~/.i3
+ln -sv $DIR/.screenlayout ~/.screenlayout
+ln -sv $DIR/.vim ~/.vim
+ln -sv $DIR/.vimrc ~/.vimrc
 
-ln -sv DIR/redshift.conf ~/.config/redshift.conf
+ln -sv $DIR/redshift.conf ~/.config/redshift.conf
