5df711ec584063cfbb780fc578257b998c2ef09f
jason <JasonTam22@gmail.com>
2015-11-06 00:03:13 -0500

    wlan and eth name changes and some other random with new system

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-xrandr \
-	--output DVI-I-2 --mode 1680x1050 --pos 0x0 --rotate left \
-	--output DVI-I-3 --mode 1680x1050 --pos 1050x328 --rotate normal --primary \
-	--output DVI-I-1 --off \
-	--output DVI-I-0 --off \
-	--output TV-0 --off
+xrandr --output HDMI3 --mode 1680x1050 --pos 0x0 --rotate left --output HDMI2 --primary --mode 1680x1050 --pos 1050x448 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off --output DP1 --off
